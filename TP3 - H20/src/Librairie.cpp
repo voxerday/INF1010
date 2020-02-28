71a09488562f16e576a26e2d690d0350c69fff72
Mart <cjamesmart@gmail.com>
2020-02-28 02:19:27 -0500

    worked a bit on lib

--- TP3 - H20/src/Librairie.cpp
+++ TP3 - H20/src/Librairie.cpp
@@ -1,20 +1,23 @@
 #include "Librairie.h"
+#include "GestionnaireAuteurs.h"
 
 // To do
 Librairie::Librairie(const Librairie& librairie)
+    :medias_()
 {
     // To do
     //std::vector<std::unique_ptr<Media>> medias_
-    medias_.reserve(librairie.medias_.size());
-
-    for (const auto& e : librairie.medias_)
-        saisons_.push_back(std::make_unique<Media>(*e));
+    *this = librairie;
 }
 
 // To do
 Librairie& Librairie::operator=(const Librairie& librairie)
 {
     // To do
+    medias_.reserve(librairie.medias_.size());
+
+    for (const auto& e : librairie.medias_)
+        medias_.push_back(std::make_unique<Media>(*e));
 }
 
 //! Destructeur de la classe Librairie
@@ -27,24 +30,44 @@ Librairie::~Librairie()
 Film* Librairie::chercherFilm(const std::string& nomFilm)
 {
     // To do
+    for (auto& m : medias_)
+    {
+        if (m->getNom() == nomFilm)
+        {
+            return (Film*)&m;
+        }
+    }
+    return nullptr;
 }
 
 // To do
 Serie* Librairie::chercherSerie(const std::string& nomSerie)
 {
     // To do
+    for (auto& s : medias_)
+    {
+        if (s->getNom() == nomSerie)
+        {
+            return (Serie*)&s;
+        }
+    }
+    return nullptr;
 }
 
 // To do
 void Librairie::ajouterSaison(const std::string& nomSerie, std::unique_ptr<Saison> saison)
 {
     // To do
+    Serie* serie = chercherSerie(nomSerie);
+    serie->operator+= *saison;
 }
 
 // To do
 void Librairie::retirerSaison(const std::string& nomSerie, unsigned int numSaison)
 {
     // To do
+    Serie* serie = chercherSerie(nomSerie);
+    serie->operator-= *numSaison;
 }
 
 // To do
@@ -52,6 +75,8 @@ void Librairie::ajouterEpisode(const std::string& nomSerie, unsigned int numSais
                                std::unique_ptr<Episode> episode)
 {
     // To do
+    Serie* serie = chercherSerie(nomSerie);
+    serie->ajouterEpisode(numSaison, std::move(episode));
 }
 
 
@@ -59,6 +84,8 @@ void Librairie::retirerEpisode(const std::string& nomSerie, unsigned int numSais
                                unsigned int numEpisode)
 {
     // To do
+    Serie* serie = chercherSerie(nomSerie);
+    serie->retirerEpisode(numSaison, numEpisode);
 }
 
 //! Méthode qui charge les series à partir d'un fichier.
@@ -70,6 +97,19 @@ bool Librairie::chargerMediasDepuisFichier(const std::string& nomFichier,
                                            GestionnaireAuteurs& gestionnaireAuteurs)
 {
     // To do
+    std::ifstream fichier(nomFichier);
+    if (fichier)
+    {
+        std::string ligne;
+        while (getline(fichier, ligne))
+            if (!lireLigneMedia(ligne, gestionnaireAuteurs))
+                return false;
+
+        return true;
+    }
+    std::cerr << "Le fichier " << nomFichier
+        << " n'existe pas. Assurez vous de le mettre au bon endroit.\n";
+    return false;
 }
 
 //! Méthode qui charge les restrictions des series à partir d'un fichier.
@@ -99,42 +139,88 @@ bool Librairie::chargerRestrictionsDepuisFichiers(const std::string& nomFichier)
 size_t Librairie::getNbMedias() const
 {
     // To do
+    return medias_.size();
 }
 
 // To do
 std::ostream& operator<<(std::ostream& os, const Librairie& librairie)
 {
     // To do
+    for (auto& media : librairie.medias_)
+    {
+        os << media << std::endl;
+    }
 }
 
 // To do
 size_t Librairie::trouverIndexMedia(const std::string& nomMedia) const
 {
     // To do
+    for (size_t i = 0; i < getNbMedias(); i++) 
+    {
+        if (medias_[i]->getNom() == nomMedia)
+        {
+            return i;
+        }
+    }
+    return size_t(-1);
 }
 
 // To do
 Librairie& Librairie::operator+=(std::unique_ptr<Media> media)
 {
     // To do
+    medias_.push_back(media);
 }
 
 // To do
 Librairie& Librairie::operator-=(const std::string& nomMedia)
 {
     // To do
+    medias_.erase(medias_.begin() + trouverIndexMedia(nomMedia));
 }
 
 // To do
 Media* Librairie::chercherMedia(const std::string& nomMedia, Media::TypeMedia typeMedia)
 {
     // To do
+    if (typeMedia == Media::TypeMedia::Film)
+    {
+        return (Media*)chercherFilm(nomMedia);
+    }
+    else
+    {
+        return (Media*)chercherSerie(nomMedia);
+    }
 }
 
 // To do
 bool Librairie::lireLigneRestrictions(const std::string& ligne)
 {
     // To do
+    std::istringstream stream(ligne);
+    int type;
+    std::string nomMedia;
+    // Pour extraire tout ce qui se trouve entre "" dans un stream,
+    // il faut faire stream >> std::quoted(variable)
+    if (stream >> type >> std::quoted(nomMedia))
+    {
+        
+        Media* media = chercherMedia(nomMedia, to_enum<Media::TypeMedia>(type));
+        if (media == nullptr)
+        {
+            // Film n'existe pas
+            return false;
+        }
+
+        int paysValeurEnum;
+        while (stream >> paysValeurEnum)
+        {
+            media->ajouterPaysRestreint(to_enum<Pays>(paysValeurEnum));
+        }
+        return true;
+    }
+    return false;
 }
 
 // To do
@@ -157,12 +243,19 @@ bool Librairie::lireLigneMedia(const std::string& ligne, GestionnaireAuteurs& ge
 const std::vector<std::unique_ptr<Media>>& Librairie::getMedias() const
 {
     // To do
+    return medias_;
 }
 
 // To do
 bool Librairie::lireLigneEpisode(std::istream& is, GestionnaireAuteurs&)
 {
     // To do
+    std::string auteur, nom;
+    int annee, genreVal, paysVal;
+    bool restriction;
+    is >> std::quoted(auteur) >> std::quoted(nom) >> annee >> genreVal >> paysVal >> restriction;
+    Auteur* dude = GestionnaireAuteurs::chercherAuteur(auteur);
+    Media();
 }
 
 // To do
